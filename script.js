02678e3df16190a8857089cf3b83b71ccec4056e
Eduardo Sucupira <eduardosucupira3@gmail.com>
2022-09-08 03:38:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const RandomNumber = ()=>{
+    const elementNumber = document.querySelector("div.generator-container h1")
+    const btn = document.querySelector("div.generator-container button")
+    let animation
+
+    btn.onclick = () =>{
+        btn.classList.toggle("sort")
+        
+        animation = setInterval(() => {
+            //Math.random gera um numero aleatorio no intervalo de ]0 a 1[
+            elementNumber.innerHTML = Math.floor(Math.random() * 10 + 1)
+       }, 20)
+
+       setTimeout(() => {
+            clearInterval(animation)
+            btn.classList.toggle("sort")
+       }, 1000)
+    }
+}
+
+window.onload = RandomNumber()
