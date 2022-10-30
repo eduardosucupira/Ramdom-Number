f3436fbba21f86107367e452276c298832574b42
Eduardo Sucupira <107318180+eduardosucupira@users.noreply.github.com>
2022-10-30 01:55:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const RandomNumber = ()=>{
+const RandomNumber = (interval, duration) => {
     const elementNumber = document.querySelector("div.generator-container h1")
     const btn = document.querySelector("div.generator-container button")
     let animation
 
-    btn.onclick = () =>{
+    btn.onclick = () =>
+    
         btn.classList.toggle("sort")
-        
         animation = setInterval(() => {
             //Math.random gera um numero aleatorio no intervalo de ]0 a 1[
-            elementNumber.innerHTML = Math.floor(Math.random() * 10 + 1)
+            elementNumber.innerHTML = Math.floor(Math.random() * interval + 1)
        }, 20)
 
        setTimeout(() => {
@@ -18,4 +18,4 @@ const RandomNumber = ()=>{
     }
 }
 
-window.onload = RandomNumber()
+window.onload = RandomNumber(10, 1000)
